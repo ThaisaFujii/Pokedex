c25c27e5b688944b86ecf984146071df52557201
ThaisaFujii <amanda_fujii@live.com>
2022-07-08 15:03:42 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Thaisa Fujii on 08/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
